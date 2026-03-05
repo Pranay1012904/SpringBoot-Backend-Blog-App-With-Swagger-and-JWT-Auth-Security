b158cc0222de861181b23e0ff4978fc5832f147b
Pranay1012904 <pranay.dmnsns@gmail.com>
2026-03-05 14:11:17 +0530

    first commit

---
+++ src/main/java/com/blogApp/blog_app_new/BlogAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.blogApp.blog_app_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogApp/blog_app_new/BlogAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogApp.blog_app_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
